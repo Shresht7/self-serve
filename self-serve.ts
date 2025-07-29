69abfc659adefa28f76c0f1ad043c3f268773ccc
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-07-29 18:44:17 +0530

    Spin up the server

--- self-serve.ts
+++ self-serve.ts
@@ -1,1 +1,38 @@
-console.log('🦕 Hello Deno World! 🦖')
+// ----
+// SELF
+// ----
+
+class Self {
+    constructor(
+        private dir: string,
+        private host: string,
+        private port: number
+    ) { }
+
+    serve() {
+        const handler = (_req: Request): Response => {
+            return new Response("Hello World")
+        }
+
+        Deno.serve({
+            hostname: this.host,
+            port: this.port
+        }, handler)
+
+        console.log(`Server started on http://${this.host}:${this.port}`)
+    }
+}
+
+// ----
+// MAIN
+// ----
+
+async function main() {
+    const self = new Self(".", "localhost", 8080)
+    await self.serve()
+}
+
+// This file is being run directly as the main program
+if (import.meta.main) {
+    main()
+}
