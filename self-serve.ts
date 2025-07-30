2b9cb30d77b3575d26bb527e1fde0de5efb92f4c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-07-30 21:08:10 +0530

    Implement system color-scheme for 404 page

--- self-serve.ts
+++ self-serve.ts
@@ -358,11 +358,16 @@ class Self {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>404 Not Found</title>
                 <style>
+                    :root { color-scheme: light dark; }
                     *, *:before, *:after { box-sizing: border-box; margin: 0; padding: 0; }
                     body { height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center; font-family: sans-serif; text-align: center; color: #333; }
                     h1 { font-size: 120px; margin: 0; font-weight: 900; }
                     p { font-size: 24px; }
                     code { background: #eee; padding: 2px 6px; border-radius: 4px; }
+                    @media (prefers-color-scheme: dark) {
+                        body { color: #eee; background-color: #333 }
+                        code { background: #777; padding: 2px 6px; border-radius: 4px; }
+                    }
                 </style>
             </head>
             <body>
