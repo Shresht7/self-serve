7b8eb26fd0b67453ad37b25740b0ce0c60399dc3
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-07-31 06:45:50 +0530

    Create `getExtension` helper function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,8 +6,13 @@ export function getClientIP(req: Request): string {
     return req.headers.get('x-forwarded-for') || req.headers.get('x-real-ip') || '127.0.0.1'
 }
 
+/** Helper function to get the extension of a file */
+export function getExtension(path: string): string {
+    return path.split('.')?.pop()?.toLowerCase() ?? ''
+}
+
 /** Helper function to get the mime-type of a file */
 export function getMimeType(path: string) {
-    const ext = path.split('.').pop() ?? ''
+    const ext = getExtension(path)
     return contentType(ext) || 'application/octet-stream'
 }
