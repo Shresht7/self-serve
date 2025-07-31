4bb6d33a2cf83d2d732fec4c35e17f27ff1734d6
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-07-31 08:11:06 +0530

    Use `join` and `extname` from the standard library

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,5 @@
 // Library
+import { extname } from "jsr:@std/path"
 import { contentType } from "jsr:@std/media-types/content-type"
 
 /** Helper function to get the IP Address of the client */
@@ -6,13 +7,8 @@ export function getClientIP(req: Request): string {
     return req.headers.get('x-forwarded-for') || req.headers.get('x-real-ip') || '127.0.0.1'
 }
 
-/** Helper function to get the extension of a file */
-export function getExtension(path: string): string {
-    return path.split('.')?.pop()?.toLowerCase() ?? ''
-}
-
 /** Helper function to get the mime-type of a file */
 export function getMimeType(path: string) {
-    const ext = getExtension(path)
+    const ext = extname(path).substring(1)
     return contentType(ext) || 'application/octet-stream'
 }
